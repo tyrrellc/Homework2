6b674fb95c0fdf28b5cf36b5664947de29b26031
Colin Tyrrell <Colin@0587337735.wireless.umich.net>
2016-11-16 09:07:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/colin/homework2/Homework2.java
@@ -0,0 +1,13 @@
+package com.example.colin.homework2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Homework2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework2);
+    }
+}
